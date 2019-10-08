5d2f4f49a8623b97e2faa6a6b66e7cf94f03d005
Christian Donker <530300@student.glu.nl>
2019-10-08 09:05:47 +0200

    haha

--- Buzzed Out/Assets/_Scripts/Swatter.cs
+++ Buzzed Out/Assets/_Scripts/Swatter.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Swatter : MonoBehaviour
 {
+    [SerializeField] private KeyCode forward;
+    [SerializeField] private KeyCode backward;
+    [SerializeField] private KeyCode left;
+    [SerializeField] private KeyCode right;
+
+    [SerializeField] private int Speed;
     void Start()
     {
         
@@ -11,6 +17,27 @@ public class Swatter : MonoBehaviour
 
     void Update()
     {
-        
+        SwatterMovement();
+    }
+
+
+    void SwatterMovement()
+    {
+        if (Input.GetKey(forward))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + (Speed * Time.deltaTime));
+        }
+        if (Input.GetKey(backward))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + -(Speed * Time.deltaTime));
+        }
+        if (Input.GetKey(left))
+        {
+            transform.position = new Vector3(transform.position.x + -(Speed * Time.deltaTime),transform.position.y , transform.position.z);
+        }
+        if (Input.GetKey(right))
+        {
+            transform.position = new Vector3(transform.position.x + (Speed * Time.deltaTime), transform.position.y, transform.position.z);
+        }
     }
 }
