dbcdb1bc28da6c9b518ff56d41533a066d97b88a
beeri2001 <35770704+beeri2001@users.noreply.github.com>
2019-11-12 10:51:44 +0100

    push

---
+++ Buzzed Out/Assets/_Scripts/PoolItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PoolItem : MonoBehaviour
+{
+    private ItemPool mPool;
+    public ItemPool pool { set { mPool = value; } }
+
+    protected abstract void Reset();
+
+    public virtual void Initialize(Vector3 position, Quaternion rotation, Transform parent)
+    {
+        Reset();
+        gameObject.SetActive(true);
+        transform.position = position;
+        transform.rotation = rotation;
+    }
+
+    public virtual void ReturnToPool()
+    {
+        mPool.AddItemToPool(this);
+    }
+}
